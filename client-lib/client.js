15cc1999217fc8ae163a5cc7aef9169dcc7a0d3f
David Kornel <dkornel@redhat.com>
2017-05-23 15:54:53 +0200

    NEW: Added main js file

---
+++ client-lib/client.js
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2015 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+var ConnectorClient = require('../client-lib/connectorClient.js').Connector;
+var ReceiverClient = require('../client-lib/receiverClient.js').Receiver;
+var SenderClient = require('../client-lib/senderClient.js').Sender;
+
+exports.SenderClient = new SenderClient();
+exports.ReceiverClient = new ReceiverClient();
+exports.ConnectorClient = new ConnectorClient();
